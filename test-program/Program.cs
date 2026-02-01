cea94f2eff4fe06cd3c9dd1fce1a47adf4111b61
Eleanor <eleanor.deleon@outlook.com>
2026-02-01 15:00:19 +0100

    idk what to do test

---
+++ test-program/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+if (!System.IO.Directory.Exists("C:/temp"))
+{
+    System.IO.Directory.CreateDirectory("C:/temp");
+}
+
+System.IO.File.WriteAllText("C:/temp/Georg.txt", DateTime.Now.ToString());
